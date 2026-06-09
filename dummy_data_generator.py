f85143d42aa8c56b11a59c748767a7707716703f
utkarsh <utkarsh8377@gmail.com>
2026-06-09 10:52:50 +0530

    json output of drift report

--- dummy_data_generator.py
+++ dummy_data_generator.py
@@ -14,13 +14,17 @@ df1= pd.DataFrame({
 )
 
 df2 = pd.DataFrame({
-  "age": np.random.randint(18, 65, n),
-  "salary": np.random.randint(10000,500000, n),
+  "age": np.random.randint(30, 65, n), # age drift 
+  "salary": np.random.randint(30000,500000, n), # little bit of salary drift
   "experience": np.random.randint(0,20,n)
 
 })
 
 df1.to_csv("data/reference.csv", index=False)
 df2.to_csv("data/current.csv", index=False)
+
+
+
+# threshold changes due to built in Bonferroni correction
 
 
--- ks_drift_pipeline.py
+++ ks_drift_pipeline.py
@@ -1,29 +1,43 @@
 import pandas as pd
 import numpy as np
 from alibi_detect.cd import KSDrift
+import warnings
 
-ref_df = pd.read_csv("data/reference.csv")
-cur_df = pd.read_csv("data/current.csv")
+warnings.filterwarnings('ignore', module='alibi_detect')
 
-x_ref = ref_df.values
-x_curr = cur_df.values
+def detect_tabular_shift(ref_path: str, curr_path: str, p_val_threshold: float = 0.05):
+  ref_df = pd.read_csv(ref_path)
+  curr_df = pd.read_csv(curr_path)
 
+  feature_names = ref_df.columns.to_list()
 
-cd = KSDrift(
-  x_ref=x_ref,
-  p_val = 0.05
-)
+  cd = KSDrift(
+    x_ref = ref_df.values,
+    p_val = p_val_threshold
+  )
 
-preds = cd.predict(
-  x_curr,
-  return_p_val= True,
-  return_distance = True 
-)
+  preds = cd.predict(
+    curr_df.values,
+    return_p_val = True,
+    return_distance = True
+  )
 
-print("Drift Detected:", preds["data"]["is_drift"])
-print("P-values:", preds["data"]["p_val"])
-print("KS Statistics:", preds["data"]["distance"])
-print("Threshold:", preds["data"]["threshold"])
+  report ={
+    "dataset_drift": bool(preds["data"]["is_drift"]==1),
+    "features": {}
+  }
 
+  for i, feature in enumerate(feature_names):
+    report["features"][feature]={
+      "drift_detected": bool(preds["data"]["p_val"][i]<p_val_threshold),
+      "p_value": float(preds["data"]["p_val"][i]),
+      "ks_stats": float(preds["data"]["distance"][i])
+    }
+  return report
 
-
+if __name__ == "__main__":
+    drift_report = detect_tabular_shift("data/reference.csv", "data/current.csv")
+    
+    # Pretty print the dictionary so it looks like a JSON API response
+    import json
+    print(json.dumps(drift_report, indent=4))
