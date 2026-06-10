2fce294d8f1b953c0cb191e064a5a81f7a5e65b4
utkarsh <utkarsh8377@gmail.com>
2026-06-10 16:33:42 +0530

    small changes

--- playground/sqlalchemy_scratch.py
+++ playground/sqlalchemy_scratch.py
@@ -8,3 +8,14 @@ conn = engine.connect()
 conn.execute(text("CREATE TABLE IF NOT EXISTS people (name str, age int)"))
 
 conn.commit()
+
+
+from sqlalchemy.orm import Session
+
+session = Session(engine)
+
+session.execute(text('INSERT INTO people (name, age) VALUES ("utkarsh", 22);'))
+
+session.commit()
+
+# this is the most basic way to work with sqlalchemy 
